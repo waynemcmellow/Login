a76236484f10ec1337348c27ee99a5935b2135fb
waynemcmellow <waynemcmellow@gmail.com>
2026-07-03 14:52:10 +0200

    Add files via upload

---
+++ Login.py
@@ -0,0 +1,9 @@
+Email = input("Enter your email ")
+Password = input("Enter your password ")
+email = "waynemcmellow@gmail.com"
+Password = "12345678"
+
+if Email == email and Password == Password:
+    print("welcome wayne")
+else:
+    print("login failed")
